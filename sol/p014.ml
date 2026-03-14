0d3ccc3a72123da3076ddc718541f081b0ac6c85
张睿 Zhang Rui <zrui16@hotmail.com>
2026-03-14 00:47:21 -0700

    Simplify 14

--- sol/p014.ml
+++ sol/p014.ml
@@ -3,22 +3,18 @@ module IntHashtbl = Hashtbl.Make (Int)
 let longest_collatz_chain n =
   let memory = IntHashtbl.create n in
   IntHashtbl.add memory 1 1;
-  let rec collatz_length x =
-    let memorize x =
+  let rec collatz_length acc x =
+    if x mod 2 = 0 then collatz_length (acc + 1) (x / 2)
+    else
       match IntHashtbl.find_opt memory x with
-      | Some len -> len
+      | Some len -> acc + len
       | None ->
-          let len = 1 + collatz_length ((3 * x) + 1) in
+          let len = collatz_length 2 (((3 * x) + 1) / 2) in
           IntHashtbl.add memory x len;
-          len
-    in
-    let rec aux acc x =
-      if x mod 2 = 0 then aux (acc + 1) (x / 2) else acc + memorize x
-    in
-    aux 0 x
+          acc + len
   in
   Seq.ints 1
-  |> Seq.map (fun x -> (collatz_length x, x))
+  |> Seq.map (fun x -> (collatz_length 0 x, x))
   |> Seq.take n
   |> Seq.fold_left max (0, 0)
 
