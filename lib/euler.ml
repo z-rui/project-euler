175063ca21be9c3d76feca87c1fb91806525d26f
张睿 Zhang Rui <zrui16@hotmail.com>
2026-03-14 12:24:46 -0700

    z_digits

--- lib/euler.ml
+++ lib/euler.ml
@@ -47,3 +47,10 @@ let factorize n =
     else next (divisor + 1) n
   in
   fun () -> next 2 n
+
+let z_digits =
+  Seq.unfold (fun z ->
+      if Z.(equal zero z) then None
+      else
+        let q, r = Z.(div_rem z ~$10) in
+        Some (Z.to_int r, q))
--- sol/p016.ml
+++ sol/p016.ml
@@ -1,10 +1,3 @@
-let power_digit_sum b e =
-  let rec loop acc x =
-    if Z.(equal zero x) then acc
-    else
-      let q, r = Z.(div_rem x ~$10) in
-      loop (acc + Z.to_int r) q
-  in
-  loop 0 (Z.pow (Z.of_int b) e)
-
-let () = power_digit_sum 2 1000 |> Printf.printf "%d\n"
+let () =
+  Z.(~$2 ** 1000)
+  |> Euler.z_digits |> Seq.fold_left ( + ) 0 |> Printf.printf "%d\n"
