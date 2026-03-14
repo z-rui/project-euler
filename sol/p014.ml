a27154a36b0c9561f8ead77bff18e637cf15549a
张睿 Zhang Rui <zrui16@hotmail.com>
2026-03-14 11:07:59 -0700

    Use array in 14

--- sol/p014.ml
+++ sol/p014.ml
@@ -1,17 +1,16 @@
-module IntHashtbl = Hashtbl.Make (Int)
-
 let longest_collatz_chain n =
-  let memory = IntHashtbl.create n in
-  IntHashtbl.add memory 1 1;
+  let memory = Array.make (n + 1) 0 in
+  memory.(1) <- 1;
   let rec collatz_length acc x =
     if x mod 2 = 0 then collatz_length (acc + 1) (x / 2)
+    else if x > n then collatz_length (acc + 2) (((3 * x) + 1) / 2)
     else
-      match IntHashtbl.find_opt memory x with
-      | Some len -> acc + len
-      | None ->
+      match memory.(x) with
+      | 0 ->
           let len = collatz_length 2 (((3 * x) + 1) / 2) in
-          IntHashtbl.add memory x len;
+          memory.(x) <- len;
           acc + len
+      | len -> acc + len
   in
   Seq.ints 1
   |> Seq.map (fun x -> (collatz_length 0 x, x))
