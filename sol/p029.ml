a3b7d408e6cf66ee0b2874d3070fcc602d8c21ee
张睿 Zhang Rui <zrui16@hotmail.com>
2026-03-14 23:50:13 -0700

    Simplify to use list

--- sol/p029.ml
+++ sol/p029.ml
@@ -1,12 +1,10 @@
 (** Distinct Powers *)
 
-module ZSet = Set.Make (Z)
-
 let distinct_powers a_max b_max =
   let rec aux acc a b =
     if b > b_max then if a = a_max then acc else aux acc (a + 1) 2
-    else aux (ZSet.add Z.(~$a ** b) acc) a (b + 1)
+    else aux (Z.(~$a ** b) :: acc) a (b + 1)
   in
-  aux ZSet.empty 2 2
+  List.sort_uniq Z.compare (aux [] 2 2)
 
-let () = distinct_powers 100 100 |> ZSet.cardinal |> Printf.printf "%d\n"
+let () = distinct_powers 100 100 |> List.length |> Printf.printf "%d\n"
