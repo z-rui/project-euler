cc38db4e678c862da9fb05654aacdb5be34446db
张睿 Zhang Rui <zrui16@hotmail.com>
2026-03-15 22:49:33 -0700

    Terser

--- sol/p046.ml
+++ sol/p046.ml
@@ -1,23 +1,17 @@
 let prime_table = Euler.Prime_table.create 10_000
 
-let goldbach n =
-  let rec loop i =
+let rec search n =
+  let rec check i =
     let diff = n - prime_table.primes.(i) in
     if diff < 0 then false
-    else if diff mod 2 = 0 && Z.perfect_square (Z.of_int (diff / 2)) then true
-    else loop (i + 1)
+    else if Z.perfect_square (Z.of_int (diff / 2)) then true
+    else check (i + 1)
   in
-  loop 0
-
-let search () =
-  let rec aux i =
-    if i >= Array.length prime_table.is_prime then
-      failwith "run out of primes; extend the prime table."
-    else if not (prime_table.is_prime.(i) || goldbach i) then i
-    else aux (i + 2)
-  in
-  aux 9
+  if n >= Array.length prime_table.is_prime then
+    failwith "run out of primes; extend the prime table."
+  else if not (prime_table.is_prime.(n) || check 1) then n
+  else search (n + 2)
 
 let () =
-  search () |> print_int;
+  search 9 |> print_int;
   print_newline ()
