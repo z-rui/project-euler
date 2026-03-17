89cb6636e778ee3f57575cc3d75e8f6581e787fe
张睿 Zhang Rui <zrui16@hotmail.com>
2026-03-17 11:07:02 -0700

    Store patterns in hex instead of string

--- sol/p051.ml
+++ sol/p051.ml
@@ -1,56 +1,44 @@
 (** Prime Digit Replacements *)
 
-module H = Hashtbl.Make (String)
-
 let prime_table = Euler.Prime_table.create 1_100_000
-let max_prime = prime_table.primes.(Array.length prime_table.primes - 1)
 
-let next_prime =
-  let arr = Array.make max_prime 0 in
-  let rec gen i j p =
-    Array.fill arr i (p - i) p;
-    if p < max_prime then gen p (j + 1) prime_table.primes.(j + 1)
+let mask_off n posmask f =
+  let rec aux acc seen i = function
+    | 0 -> f acc
+    | n ->
+        let d = n mod 10 in
+        let shift = 4 * i in
+        let i' = i + 1 and n' = n / 10 in
+        if (1 lsl i) land posmask <> 0 then begin
+          let seen' = 1 lsl d in
+          if seen' lor seen = seen' then
+            aux ((0xf lsl shift) lor acc) seen' i' n'
+        end
+        else aux ((d lsl shift) lor acc) seen i' n'
   in
-  gen 0 0 2;
-  arr
+  aux 0 0 0 n
 
-let[@inline] char_to_digit c = Char.code c - Char.code '0'
-
-let all_same_char s posmask =
-  let n = String.length s in
-  let rec loop seen i =
-    if i = n then true
-    else
-      let b = 1 lsl i in
-      if b land posmask <> 0 then
-        let digit_mask = 1 lsl char_to_digit s.[i] in
-        digit_mask lor seen = digit_mask && loop digit_mask (i + 1)
-      else loop seen (i + 1)
-  in
-  loop 0 0
-
-let rec search w scale family_size =
+let rec search prime_idx w scale family_size =
+  let module H = Hashtbl.Make (Int) in
   let scale' = scale * 10 in
   let patterns = H.create (1 lsl (3 * w)) in
   let allbits = (1 lsl w) - 1 in
-  let rec loop p =
-    let s = Int.to_string p in
-    for bitmask = 1 to allbits - 1 do
-      if all_same_char s bitmask then begin
-        let s' =
-          String.init w @@ fun i ->
-          if (1 lsl i) land bitmask <> 0 then '*' else s.[i]
-        in
+  let rec loop prime_idx =
+    if prime_idx >= Array.length prime_table.primes then
+      failwith "run out of primes; extend prime table";
+    let p = prime_table.primes.(prime_idx) in
+    if p >= scale' then prime_idx
+    else begin
+      for bitmask = 1 to allbits - 1 do
+        mask_off p bitmask @@ fun s' ->
         match H.find_opt patterns s' with
         | Some (n, _) -> incr n
         | None -> H.add patterns s' (ref 1, p)
-      end
-    done;
-    if p = max_prime then failwith "run out of primes; extend prime table";
-    let p' = next_prime.(p) in
-    if p' < scale' then loop p'
+      done;
+      loop (prime_idx + 1)
+    end
   in
-  loop next_prime.(scale);
+  let prime_idx' = loop prime_idx in
 
   (* All patterns of width w have been visited. *)
   match
@@ -66,8 +54,9 @@ let rec search w scale family_size =
       patterns None
   with
   | Some p -> p
-  | None -> search (w + 1) scale' family_size
+  | None -> search prime_idx' (w + 1) scale' family_size
 
 let () =
-  search 2 10 8 |> print_int;
+  (* 2 3 5 7 [11], first idx = 4 *)
+  search 4 2 10 8 |> print_int;
   print_newline ()
