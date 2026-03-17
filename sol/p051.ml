3bc53e4f01cc082efc316cf8c37e05b9ebd6632f
张睿 Zhang Rui <zrui16@hotmail.com>
2026-03-17 12:13:00 -0700

    Optimization

--- sol/p051.ml
+++ sol/p051.ml
@@ -2,39 +2,38 @@
 
 let prime_table = Euler.Prime_table.create 1_100_000
 
-let mask_off n posmask f =
-  let rec aux acc seen i = function
-    | 0 -> f acc
+let iter_patterns n f =
+  let rec aux acc used i = function
+    | 0 -> if used <> 0 then f acc
     | n ->
         let d = n mod 10 in
         let shift = 4 * i in
         let i' = i + 1 and n' = n / 10 in
-        if (1 lsl i) land posmask <> 0 then begin
-          let seen' = 1 lsl d in
-          if seen' lor seen = seen' then
-            aux ((0xf lsl shift) lor acc) seen' i' n'
-        end
-        else aux ((d lsl shift) lor acc) seen i' n'
+        let this = 1 lsl d in
+        if this lor used = this then aux ((0xf lsl shift) lor acc) this i' n';
+        aux ((d lsl shift) lor acc) used i' n'
   in
   aux 0 0 0 n
 
 let rec search prime_idx w scale family_size =
-  let module H = Hashtbl.Make (Int) in
+  let module H = struct
+    include Hashtbl.Make (Int)
+
+    type elt = { mutable cnt : int; p : int }
+  end in
   let scale' = scale * 10 in
   let patterns = H.create (1 lsl (3 * w)) in
-  let allbits = (1 lsl w) - 1 in
   let rec loop prime_idx =
     if prime_idx >= Array.length prime_table.primes then
       failwith "run out of primes; extend prime table";
     let p = prime_table.primes.(prime_idx) in
     if p >= scale' then prime_idx
     else begin
-      for bitmask = 1 to allbits - 1 do
-        mask_off p bitmask @@ fun s' ->
-        match H.find_opt patterns s' with
-        | Some (n, _) -> incr n
-        | None -> H.add patterns s' (ref 1, p)
-      done;
+      iter_patterns p begin fun pat ->
+          match H.find_opt patterns pat with
+          | Some ent -> H.(ent.cnt <- ent.cnt + 1)
+          | None -> H.(add patterns pat { cnt = 1; p })
+        end;
       loop (prime_idx + 1)
     end
   in
@@ -43,8 +42,8 @@ let rec search prime_idx w scale family_size =
   (* All patterns of width w have been visited. *)
   match
     H.fold
-      begin fun _ (cnt, p) acc ->
-        if !cnt = family_size then
+      begin fun _ H.{ cnt; p } acc ->
+        if cnt = family_size then
           match acc with
           | None -> Some p
           | Some p' when p' > p -> Some p
