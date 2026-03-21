87610e866980628dd2798fc77a4ede85ed5cdc11
张睿 Zhang Rui <zrui16@hotmail.com>
2026-03-21 15:05:35 -0700

    Commonize continued fraction

--- lib/euler.ml
+++ lib/euler.ml
@@ -113,3 +113,39 @@ module Word_file = struct
 
   let word_value s = String.fold_left (fun acc c -> acc + letter_value c) 0 s
 end
+
+module Continued_fraction = struct
+  (** Representation of numbers in the form of (sqrt(A) + B / D), whereas A, B
+      and D are integers. *)
+  module QZ = struct
+    type t = { a : Z.t; b : Z.t; d : Z.t }
+
+    let equal x y = Z.(equal x.a y.a && equal x.b y.b && equal x.d y.d)
+
+    let reci { a; b; d } =
+      let d' = Z.(a - (b * b)) in
+      let d'' = Z.divexact d' d in
+      assert (Z.(equal d' @@ (d'' * d)));
+      let i = Z.((sqrt a - b) / d'') in
+      let b' = Z.(-b - (i * d'')) in
+      (i, { a; b = b'; d = d'' })
+  end
+
+  let quadratic n =
+    let r, e = Z.sqrt_rem n in
+    if Z.(equal zero e) then (r, [])
+    else
+      let initial = QZ.{ a = n; b = Z.neg r; d = Z.one } in
+      let[@tail_mod_cons] rec loop w =
+        let i, w' = QZ.reci w in
+        if QZ.equal initial w' then [ i ] else i :: loop w'
+      in
+      (r, loop initial)
+
+  let convergent i f terms =
+    let w = ref Q.zero in
+    for i = terms - 1 downto 0 do
+      w := Q.(one / (of_bigint (f i) + !w))
+    done;
+    Q.(of_bigint i + !w)
+end
--- sol/p065.ml
+++ sol/p065.ml
@@ -1,13 +1,8 @@
 (** Convergents of e *)
 
-let convergent i f terms =
-  let w = ref Q.zero in
-  for i = terms - 1 downto 0 do
-    w := Q.(one / (of_int (f i) + !w))
-  done;
-  Q.(of_int i + !w)
-
 let () =
-  convergent 2 (fun i -> if i mod 3 = 1 then ((i / 3) + 1) * 2 else 1) 99
+  Euler.Continued_fraction.convergent (Z.of_int 2)
+    (fun i -> if i mod 3 = 1 then Z.of_int (((i / 3) + 1) * 2) else Z.one)
+    99
   |> Q.num |> Euler.z_digits |> Seq.fold_left ( + ) 0 |> print_int;
   print_newline ()
