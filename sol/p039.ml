730675bb6b473deef47bba4d4a071ad4e9209115
张睿 Zhang Rui <zrui16@hotmail.com>
2026-03-22 12:45:51 -0700

    Simplify

--- sol/p039.ml
+++ sol/p039.ml
@@ -1,5 +1,6 @@
 let () =
-  let p_max = 1000 in
-  let count = Euler.Right_triangles.count_by_perimeter p_max in
-  Array.to_seq count |> Seq.mapi (fun i x -> (x, i)) |> Seq.fold_left max (0, -1)
+  Euler.Right_triangles.count_by_perimeter 1000
+  |> Array.to_seq
+  |> Seq.mapi (fun i x -> (x, i))
+  |> Seq.fold_left max (0, -1)
   |> fun (cnt, i) -> Printf.printf "i=%d cnt=%d\n" i cnt
--- sol/p075.ml
+++ sol/p075.ml
@@ -1,7 +1,6 @@
 (** Singular Integer Right Triangles *)
 
 let () =
-  let p_max = 1_500_000 in
-  let count = Euler.Right_triangles.count_by_perimeter p_max in
-  Array.fold_left (fun acc n -> if n = 1 then acc + 1 else acc) 0 count
+  Euler.Right_triangles.count_by_perimeter 1_500_000
+  |> Array.fold_left (fun acc n -> if n = 1 then acc + 1 else acc) 0
   |> Printf.printf "%d\n"
