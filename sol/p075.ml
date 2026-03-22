c82a321abd1a3b285ba766d146b02fd096e29c7a
张睿 Zhang Rui <zrui16@hotmail.com>
2026-03-22 02:00:38 -0700

    75

---
+++ sol/p075.ml
@@ -0,0 +1,7 @@
+(** Singular Integer Right Triangles *)
+
+let () =
+  let p_max = 1_500_000 in
+  let count = Euler.count_right_triangles p_max in
+  Array.fold_left (fun acc n -> if n = 1 then acc + 1 else acc) 0 count
+  |> Printf.printf "%d\n"
