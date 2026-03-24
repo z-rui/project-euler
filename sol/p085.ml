1ccd119bcbe4de670db82e196bd6f09dcc96a44c
张睿 Zhang Rui <zrui16@hotmail.com>
2026-03-24 02:06:05 -0700

    Fix naming

--- sol/p085.ml
+++ sol/p085.ml
@@ -12,8 +12,8 @@ let () =
   let limit = Float.(sqrt (of_int target) *. 2.0 |> sqrt |> ceil |> to_int) in
   let best = ref (0, 0) in
   let best_abs_err = ref Int.max_int in
-  let test a b area =
-    let abs_err = Int.abs (area - target) in
+  let test a b rect_cnt =
+    let abs_err = Int.abs (rect_cnt - target) in
     if abs_err < !best_abs_err then begin
       best := (a, b);
       best_abs_err := abs_err
@@ -24,12 +24,12 @@ let () =
     let tb_approx = Float.(of_int target /. of_int ta) in
     let b = Float.(sqrt (tb_approx *. 2.0) |> to_int) in
     let tb = b * (b + 1) / 2 in
-    let area = ta * tb in
-    test a b area;
-    if area < target then test a (b + 1) (area + (ta * (b + 1)))
-    else if area > target then test a (b - 1) (area - (ta * b))
+    let rect_cnt = ta * tb in
+    test a b rect_cnt;
+    if rect_cnt < target then test a (b + 1) (rect_cnt + (ta * (b + 1)))
+    else if rect_cnt > target then test a (b - 1) (rect_cnt - (ta * b))
   done;
   let a, b = !best in
   let ta = a * (a + 1) / 2 and tb = b * (b + 1) / 2 in
-  Printf.printf "a=%d, b=%d, area=%d, rectangle_count= %d * %d = %d\n" a b
+  Printf.printf "a=%d, b=%d, rect_cnt=%d, rectangle_count= %d * %d = %d\n" a b
     (a * b) ta tb (ta * tb)
